76da2db332333818ea54e2c2c3fad954df9f0396
IgorIlyaguev <non_2002@mail.ru>
2025-12-29 01:42:42 +0300

    setup docker and docker-compose

---
+++ compose.mk
@@ -0,0 +1,11 @@
+compose-up:
+	docker-compose --env-file ./docker/.env up -d
+
+compose-build:
+	docker-compose --env-file ./docker/.env build
+
+compose-down:
+	docker-compose --env-file ./docker/.env down -v --remove-orphans
+
+compose-config:
+	docker-compose --env-file ./docker/.env config
